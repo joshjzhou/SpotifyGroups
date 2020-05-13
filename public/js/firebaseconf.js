83284ab489abf051804157aa993e520152199605
Josh Zhou <32548076+joshjzhou@users.noreply.github.com>
2020-05-13 16:52:19 -0400

    Remove firebase config details

--- public/js/firebaseconf.js
+++ public/js/firebaseconf.js
@@ -1,12 +1,12 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyBy8n1-BjOCaHHGjg6ZZgaQ_gp0EAFK_Tc",
-    authDomain: "spotifygroups-627c7.firebaseapp.com",
-    databaseURL: "https://spotifygroups-627c7.firebaseio.com",
-    projectId: "spotifygroups-627c7",
-    storageBucket: "spotifygroups-627c7.appspot.com",
-    messagingSenderId: "557372370976",
-    appId: "1:557372370976:web:16812293a1b236844d0c45",
-    measurementId: "G-X8GFHZPDQW"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
